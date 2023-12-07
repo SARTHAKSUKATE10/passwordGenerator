86b0806fbc339637d98e6fc67b78e6120138ce2d
jrsarthak10@gmail.com <jrsarthak10@gmail.com>
2023-12-07 19:08:52 +0530

    design created

--- passChanger/src/App.jsx
+++ passChanger/src/App.jsx
@@ -1,14 +1,75 @@
-import { useState } from 'react'
+import { useState,useCallback } from 'react'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState('')
+
+const generatePassword = useCallback(()=>{
+  let pass = ''
+  let str="ABCDEFGHJIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuv"
+})
 
   return (
-    <>
-      
-    </>
+
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-600'>
+      <h1 className='text-white text-center my-3'>Password Generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input
+          type="text"
+          value={password}
+          className='outline-none w-full py-1 px-3'
+          placeholder='Password'
+          readOnly
+        />
+        <button
+          className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >Copy</button>
+      </div>
+      <div
+        className='flex test-sm gap-x-2'
+      >
+        <div className='flex items-center gap-x-1'>
+          <input type="range"
+            min={6}
+            max={100}
+            value={length}
+            className='cursor-pointer'
+            onChange={(e) => setLength(e.target.value)}
+            name=''
+            id=''
+          />
+          <label htmlFor="length">Length: {length}</label>
+        </div>
+        <div>
+          <input 
+          type="checkbox" 
+          defaultChecked={numberAllowed}
+          onChange={()=>{
+            setNumberAllowed((prev)=>!prev)
+          }}
+          name=''
+          id=''
+          />
+          <label htmlFor="number">Number</label>
+        </div>
+        <div>
+          <input 
+          type="checkbox" 
+          defaultChecked={charAllowed}
+          onChange={()=>{
+            setCharAllowed((prev)=>!prev)
+          }}
+          name=''
+          id=''
+          />
+          <label htmlFor="charInput">Character</label>
+        </div>
+      </div>
+    </div>
   )
 }
 
